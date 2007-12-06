623ce903fd254e6a425da20545c90ff7bcb874bf
andrei.arusoaie <andrei.arusoaie@dd31d841-a040-0410-87a2-617897439bad>
2007-12-06 17:12:14 +0000

    DELETE main.c

--- trunk/main.c
+++
@@ -1,8 +0,0 @@
-/* RogueWorld main file.
- * Developers:
- *   Andrei Vasiliu (whyte.vuhuni@gmail.com)
- *   Andrei Arusoaie (Andrei.Arusoaie@gmail.com)
- * Commit 2
- */
-
-/* Initial file. Empty, for now. */
