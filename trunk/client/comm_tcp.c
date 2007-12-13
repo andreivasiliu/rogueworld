0f34efa1f4e0df1f8b33b151a4209387462baa19
whyte.vuhuni <whyte.vuhuni@dd31d841-a040-0410-87a2-617897439bad>
2007-12-13 18:57:21 +0000

    Initial client skeleton.

---
+++ trunk/client/comm_tcp.c
@@ -0,0 +1,76 @@
+/* Network code and main loop. This is the only place where platform-specific
+ * code is allowed. */
+
+
+#include <stdio.h>
+#include <unistd.h>
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+
+#include "common.h"
+
+
+int server_socket;
+
+
+int connect_to_server( char *hostname, int port )
+{
+   struct sockaddr_in server;
+   struct hostent *host;
+   int sock;
+   
+   if ( ( sock = socket( PF_INET, SOCK_STREAM, 0 ) ) < 0 )
+     {
+	perror( "socket" );
+	return 1;
+     }
+   
+   host = gethostbyname( hostname );
+   if ( !host )
+     {
+	fprintf( stderr, "Cannot resolve the host '%s'.\n", hostname );
+	return 1;
+     }
+   
+   server.sin_family = AF_INET;
+   server.sin_port = htons( port );
+   server.sin_addr = *(struct in_addr*) host->h_addr;
+   
+   if ( connect( sock, (struct sockaddr*) &server, sizeof(server) ) )
+     {
+	die( "Connection failed." );
+	return 1;
+     }
+   
+   server_socket = sock;
+   
+   return 0;
+}
+
+
+int receive_one_packet( )
+{
+   char buf[4096];
+   int bytes;
+   
+   bytes = read( server_socket, buf, 4096 );
+   if ( bytes < 0 )
+     {
+	die( "Connection error." );
+	return 1;
+     }
+   if ( bytes == 0 )
+     {
+	die( "Connection closed by server." );
+	return 1;
+     }
+   
+   parse_data( buf, bytes );
+   
+   return 0;
+}
+
+
---
+++ trunk/client/rwp_send.c
@@ -0,0 +1,13 @@
+/* RogueWorld Protocol packet maker. */
+
+
+#include "common.h"
+
+
+void send_login( char *name )
+{
+   //char buf[256];
+   
+   // ..
+}
+
