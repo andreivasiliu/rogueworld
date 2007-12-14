39952b894640fb190e84f4bf916a49de6e271dca
whyte.vuhuni <whyte.vuhuni@dd31d841-a040-0410-87a2-617897439bad>
2007-12-14 18:04:27 +0000

    RLE Compression sketch.

---
+++ server/rle.c
@@ -0,0 +1,21 @@
+/* RLE Compression. */
+
+
+/* Encodes at most 'max' bytes into dest, including the terminating zero,
+ * and returns 1 if it did not fit. */
+int rle_encode( char *src, char *dest, int max )
+{
+   
+   
+   
+   return 0;
+}
+
+
+void rle_decode( char *src, char *dest )
+{
+   
+   
+   
+}
+
