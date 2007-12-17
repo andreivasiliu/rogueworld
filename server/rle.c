97a8a24f8e2133a6fb1552415f7563a56428c179
whyte.vuhuni <whyte.vuhuni@dd31d841-a040-0410-87a2-617897439bad>
2007-12-17 12:30:35 +0000

    Minor convenience changes.

--- server/rle.c
+++ server/rle.c
@@ -3,49 +3,35 @@
 
 /* Encodes at most 'max' bytes into dest, including the terminating zero,
  * and returns 1 if it did not fit. */
-int rle_encode( unsigned char *src, unsigned char *dest, int max )
+int rle_encode( char *src, char *dest, int max )
 {
-   unsigned char c, *s1, *s2, *d;
-   unsigned int length, pos, i;
-   if ( ( src == NULL ) || ( dest == NULL ) )
+   unsigned char *s1, *s2, *d;
+   
+   s1 = s2 = (unsigned char *) src;
+   d = (unsigned char *) dest;
+   
+   while ( *s2 && max > 1 )
      {
-        perror ( "NULL arguments - rle_encode" );
-        return -1;
-     }
-   if ( max == 0 )
-     {
-        perror ( "invalid max - rle_encode" );
-        return -1;
-     }
-
-
-   s1 = s2 = src;
-   d = dest;
-   length = 0;
-   while  ( *s2 != '\0' )
-     {
-        while ( *s2 == *s1 )
+        while ( *(s2++) == *s1 )
           {
-             s2++;
              if ( s2 - s1 == 255 )
                {
-                  *d = s2-s1;
-                  d++;
-                  *d = *s1;
-                  d++;
+                  *(d++) = s2 - s1;
+                  *(d++) = *s1;
                   s1 = s2;
                }
           }
-        *d = s2 - s1;
-        d++;
-        *d = *s1;
-        d++;
+        *(d++) = s2 - s1;
+        *(d++) = *s1;
         s1 = s2;
-        if ( d - dest >= max )
-          break;
+	
+	max--;
      }
-   *d = 0;
-  return 0;
+   
+   if ( max )
+     *d = 0;
+   
+   return 0;
 }
 
 
