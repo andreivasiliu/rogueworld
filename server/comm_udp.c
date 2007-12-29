9620680191fc60059344834da968ee8e4a137eca
andrei.arusoaie <andrei.arusoaie@dd31d841-a040-0410-87a2-617897439bad>
2007-12-29 21:14:43 +0000

    it finnaly arrived... comm_udp.c

---
+++ server/comm_udp.c
@@ -0,0 +1,274 @@
+/* Network and Main Loop.
+ * This is the only file that will allow platform-specific code.
+ */
+
+
+#if !defined(WIN32)
+# include <sys/socket.h>
+# include <sys/select.h>
+# include <arpa/inet.h>
+#else
+# include <winsock2.h>
+#endif
+#include <sys/time.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+#include "common.h"
+
+
+CONN *connections;
+
+
+#if defined(WIN32)
+# define CLOSE(sock)	closesocket(sock)
+#else
+# define CLOSE(sock)	close(sock)
+#endif
+
+
+int init_server( int port )
+{
+   static struct sockaddr_in sa_zero;
+   struct sockaddr_in sa;
+   int fd, x = 1;
+   
+#if defined(WIN32)
+     {
+        /* Initialize WSA. */
+        WORD wVersionRequested;
+        WSADATA wsaData;
+        wVersionRequested = MAKEWORD( 1, 0 );
+        
+        if ( WSAStartup( wVersionRequested, &wsaData ) )
+          {
+             return 0;
+          }
+        
+        if ( LOBYTE( wsaData.wVersion ) != 1 ||
+             HIBYTE( wsaData.wVersion ) != 0 )
+          {
+             WSACleanup( );
+             return 0;
+          }
+        /* End of WSA. */
+     }
+#endif
+   
+   if ( ( fd = socket( PF_INET, SOCK_DGRAM, 0 ) ) < 0 )
+     {
+	debugf( "init_server: socket: %s.", strerror( errno ) );
+	return -1;
+     }
+
+   if ( setsockopt( fd, SOL_SOCKET, SO_REUSEADDR,
+		    (char *) &x, sizeof( x ) ) < 0 )
+     {
+	debugf( "init_server: SO_REUSEADDR: %s.", strerror( errno ) );
+	CLOSE( fd );
+	return -1;
+     }
+   
+   sa            = sa_zero;
+   sa.sin_family = AF_INET;
+   sa.sin_port   = htons( port );
+
+   if ( bind( fd, (struct sockaddr *) &sa, sizeof( sa ) ) < 0 )
+     {
+	debugf( "init_server: bind: %s.", strerror( errno ) );
+	CLOSE( fd );
+	return -1;
+     }
+
+   
+   return fd;
+}
+
+int new_connection ( int control )
+{
+   struct sockaddr_in addr;
+   unsigned int size;
+   char buf[4096];
+   int desc, new_sock, bytes;
+   CONN *c;
+   
+   size = sizeof( addr );
+   
+   /*Receive a package from a new player*/
+   if ( ( desc = recvfrom ( control, buf, 4096, 0, ( struct sockaddr * ) &addr, &size ) ) < 0 )
+     {
+	debugf ( "new_connection: recvfrom: %s.", strerror( errno ) );
+	return 1;
+     }
+   
+   /*Create a new socket for it*/
+   if ( ( new_sock = socket( PF_INET, SOCK_DGRAM, 0 ) ) < 0 )
+     {
+	debugf( "new_connection: new_socket: %s.", strerror( errno ) );
+	return -1;
+     }
+   /*Binding...*/
+   if ( connect ( new_sock, ( struct sockaddr* )&addr, size ) < 0 )
+     {
+	debugf ( "new_connection: connect: %s.", strerror ( errno ) );
+	return 1;
+     }
+   
+   /*Make a new connection*/
+   c = calloc ( 1, sizeof ( CONN ) );
+   c->sock = new_sock;
+   c->player = NULL;
+   
+   /*Link it*/
+   c->next = connections;
+   connections = c;
+   
+   /*Doing something with the package received...*/
+   parse_data( c, buf, desc );
+   
+   return 0;
+}
+
+void destroy_connection ( CONN *ze_unfortunate )
+{
+   CONN *c;
+   
+   CLOSE ( ze_unfortunate->sock );
+   
+   /*First one in the chain.*/
+   if ( connections == ze_unfortunate )
+     {
+	connections = connections->next;
+     }
+   else 
+     {
+	for ( c = connections; c; c = c->next )
+	  if ( c->next == ze_unfortunate )
+	    {
+	       c->next = ze_unfortunate->next;
+	       break;
+	    }
+     }
+   
+   free ( ze_unfortunate );
+}
+
+int read_data ( CONN *conn )
+{
+   struct sockaddr_in from;
+   char buf[4096];
+   int bytes, size;
+   
+   size = sizeof ( from );
+      
+   bytes = recvfrom ( conn->sock, buf, 4096, 0, ( struct sockaddr* ) &from, &size );
+   
+   /*Connection error.*/
+   if ( bytes < 0 )
+     {
+	
+	pl_disconnected( conn->player, 1 );
+	destroy_connection ( conn );
+	
+	return 0;
+     }
+   
+   /*Connection closed.*/
+   if ( bytes == 0 )
+     {
+	pl_disconnected ( conn->player, 0 );
+	destry_connection ( conn );
+	
+	return 0;
+     }
+   
+   parse_data ( conn, buf, bytes );
+   
+   return 0;
+}
+
+	
+void send_to_client( CONN *c, char *data, int bytes )
+{
+   send( c->sock, data, bytes, 0 );
+}
+
+
+int main_loop( int port )
+{
+   struct timeval pulsetime;
+   struct timeval lasttick, now;
+   fd_set in_set;
+   fd_set out_set;
+   fd_set exc_set;
+   int server, max_fd;
+   CONN *c;
+   
+   server = init_server( port );
+   if ( server < 0 )
+     return 1;
+   
+   gettimeofday( &lasttick, NULL );
+   
+   while ( 1 )
+     {
+	FD_ZERO( &in_set  );
+	FD_ZERO( &out_set );
+	FD_ZERO( &exc_set );
+	
+	FD_SET( server, &in_set );
+	max_fd = server;
+	
+	/* What descriptors do we want to select? */
+	for ( c = connections; c; c = c->next )
+	  {
+	     FD_SET( c->sock, &in_set );
+	     
+	     if ( max_fd < c->sock )
+	       max_fd = c->sock;
+	  }
+	
+	pulsetime.tv_sec = 0;
+	pulsetime.tv_usec = 50000;
+	
+	select( max_fd + 1, &in_set, &out_set, &exc_set, &pulsetime );
+	
+	gettimeofday( &now, NULL );
+	now.tv_usec -= 250000;
+	if ( now.tv_usec < 0 )
+	  {
+	     now.tv_usec += 1000000;
+	     now.tv_sec -= 1;
+	  }
+	
+	if ( timercmp( &lasttick, &now, < ) )
+	  {
+	     tick_event( );
+	     gettimeofday( &lasttick, NULL );
+	     continue;
+	  }
+	
+	/* New connection. */
+	if ( FD_ISSET( server, &in_set ) )
+	  {
+	     if ( new_connection( server ) )
+	       return 1;
+	  }
+	
+	/* Go through the connection list. */
+	for ( c = connections; c; c = c->next )
+	  if ( FD_ISSET( c->sock, &in_set ) )
+	    {
+	       if ( read_data( c ) )
+		 return 1;
+	       
+	       /* Don't continue.. the connections list might've changed. */
+	       break;
+	    }
+     }
+}
+
+
+  
