da21e52f35f6e725384db527fd92720a4277965e
andrei.arusoaie <andrei.arusoaie@dd31d841-a040-0410-87a2-617897439bad>
2008-01-10 09:51:10 +0000

    Temporary tests.

--- trunk/server/utils.c
+++ trunk/server/utils.c
@@ -9,6 +9,8 @@
 /* RLE Compression. */
 
 
+#include <stdio.h>
+
 /**
  * RLE Encoder
  * Encodes at most 'max' bytes into dest, including the terminating zero,
@@ -58,16 +60,9 @@ int rle_encode( char *src, char *dest, int max )
  */
 void rle_decode( unsigned char *src, unsigned char *dest )
 {
-   int i, k, j;
+/*   int i, k, j;
    i = 0;
    k = 0;
-
-   if ( ( src == NULL ) || ( dest == NULL ) )
-     {
-        perror ( "NULL arguments - rle_decode" );
-        exit(0);
-     }
-
 
    while ( i < strlen( src ) )
      {
@@ -79,9 +74,119 @@ void rle_decode( unsigned char *src, unsigned char *dest )
         i+=2;
         if ( src[i] == '\0' )
           break;
-     }
+     }*/
 }
 
 
 
+#if 0
+void Bresenham_old(short x1, short y1, short x2, short y2,
+	       void (*delegate)( short y, short x, void *p ), void *p )
+{
 
+   short slope, dx, dy, incE, incNE, d, x, y;
+   
+   //y1 = -y1;
+   //y2 = -y2;
+   
+   // Reverse lines where x1 > x2
+   if (x1 > x2)
+     {
+	Bresenham(x2, y2, x1, y1, delegate, p);
+	return;
+     }
+   dx = x2 - x1;
+   dy = y2 - y1;
+   // Adjust y-increment for negatively sloped lines
+   if (dy < 0)
+     {
+	slope = -1;
+	dy = -dy;
+     }
+   else
+     {
+	slope = 1;
+     }
+   // Bresenham constants
+   incE = 2 * dy;
+   incNE = 2 * dy - 2 * dx;
+   d = 2 * dy - dx;
+   y = y1;
+   // Blit
+   for (x = x1; x <= x2; x++)
+     {
+	delegate( y, x, p );
+	if (d <= 0)
+	  {
+	     d += incE;
+	  }
+	else
+	  {
+	     d += incNE;
+	     y += slope;
+	  }
+     }
+}
+#endif
+
+void Bresenham(short x0, short y0, short x1, short y1,
+	       void (*delegate)( short y, short x, void *p ), void *p )
+{
+#define SWAP(a, b)	(temp) = (a), (a) = (b), (b) = (temp)
+#define ABS(a)		((a) < 0 ? -(a) : (a))
+   
+   int deltax, deltay, error, ystep, y, x;
+   
+   int steep = ABS( y1 - y0 ) > ABS( x1 - x0 );
+   int temp, inverse = 0;
+   
+   if ( steep ) 
+     {
+	SWAP( x0, y0 );
+	SWAP( x1, y1 );
+     }
+   
+   printf( "Steep: %d, x0>x1: %d\n", steep, x0 > x1 );
+   
+   if ( x0 > x1 )
+     {
+	//SWAP( x0, x1 );
+	//SWAP( y0, y1 );
+	inverse = 1;
+     }
+   
+   deltax = x1 - x0;
+   deltay = ABS ( y1 - y0 );
+   error = ( -deltax ) / 2;
+   y = y0;
+   if ( y0 < y1 )
+     ystep = 1;
+   else
+     ystep = -1;
+   
+   for ( x = x0; x <= x1; x++ )
+     {
+	if ( steep )
+	  {
+	     if ( !inverse )
+	       delegate( x, y, p );
+	     else
+	       delegate( -x, -y, p );
+	  }
+	else
+	  {
+	     if ( !inverse )
+	       delegate( y, x, p );
+	     else
+	       delegate( -y, -x, p );
+	  }
+	
+	error = error + deltay;
+	
+	if ( error >= 0 )
+	  {
+	     y = y + ystep;
+	     error = error - deltax;
+	  }
+     }
+}
